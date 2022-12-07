b8fe962aea1c0a3b0fc57cbebca77b4e3d7325ae
hieunn301 <95908932+hieunn301@users.noreply.github.com>
2022-12-07 17:30:00 +0700

    check map

--- oop/workspace/Main.java
+++ oop/workspace/Main.java
@@ -2,59 +2,50 @@ package oop.workspace;
 
 class Main {
     public static void main(String[] args) {
-//        Stack stack = new Stack();
-//        stack.push(11);
-//        stack.push(22);
-//        stack.push(33);
-//        stack.push(44);
-//        System.out.println(stack);
-//        stack.pop();
-//        stack.peek();
-//        stack.size();
-//        stack.pop();
-
-//        Queue queue = new Queue();
-//        queue.enqueue(11);
-//        queue.enqueue(22);
-//        queue.enqueue(33);
-//        queue.enqueue(44);
-//        queue.enqueue(55);
-//        queue.size();
-//        queue.first();
-//        queue.dequeue();
-//        queue.first();
-//        queue.size();
+        Map map = new Map() {
+            map.add(12, 11);
+            map.add(22, 11);
+            map.add(33, 11);
+            map.add(44, 11);
+            map.add(55, 11);
+            map.add(66, 11);
+            map.add(77, 11);
+            map.remove(77);
+            map.containsKey(77);
+            System.out.println(map);
+//        System.out.println(map);
+    }
+}
 
 
-        Map<Integer, Integer> map = new Map<Integer, Integer>();
-        map.put(0, 0);
-        map.put(1, 1);
-        map.put(2, 2);
-        map.put(3, 3);
-        map.put(4, 4);
-        map.put(5, 5);
-        map.put(6, 6);
-        map.put(7, 7);
-        map.put(8, 8);
-        map.put(9, 9);
-        map.put(10, 10);
-        map.put(11, 11);
-        map.put(12, 12);
-        map.put(13, 13);
-        map.put(14, 14);
-        map.put(15, 15);
-        map.put(16, 16);
-        map.display();
-        map.remove(1);
-        map.remove(2);
-        map.remove(3);
-        map.display();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        map.remove(2);
+//        map.remove(3);
+//        map.display();
 
 
 
 //        System.out.println("list: " + );
 
-    }
 //        Scanner scanner = new Scanner(System.in);
 //
 //        System.out.print("Enter id: ");
@@ -96,5 +87,4 @@ class Main {
 //        User.addUser(user);
 //        System.out.println(user.toString());
 //        user.getId();
-    }
 
--- oop/workspace/custom.java
+++ oop/workspace/custom.java
@@ -1,143 +1,202 @@
 package oop.workspace;
 
-class Node{
-    public int data;
-    public Node nextNode;
-    public Node(int data){this.data = data;}
-    public int getData() {return data;}
-    public Node getNextNode() {return nextNode;}
-    public void setNextNode(Node nextNode) {this.nextNode = nextNode;}
-}
-class Stack{
-    private int size = 0;
-    Node top = null;
-    public boolean isEmpty(){return size == 0;}
-    public void size(){
-        System.out.println("Size: " + size);
-    }
-    public void push(int data) {
-        Node newNode = new Node(data);
-        newNode.setNextNode(top);
-        top = newNode;
-        size++;
-        System.out.println("Add: " + data);
-    }
-    public void pop() {
-        if (isEmpty()) {
-            System.out.println("Empty");
-        } else {
-            Node node = top;
-            top = top.getNextNode();
-            size--;
-            System.out.println("Remove: " + node.getData());
-        }
-    }
-    public void peek(){
-        if (isEmpty()) {
-            System.out.println("Empty");
-        } else {
-            System.out.println("Top: " + top.getData());
-        }
-    }
-}
+//class Node{
+//    public int data;
+//    public Node nextNode;
+//    public Node(int data){this.data = data;}
+//    public int getData() {return data;}
+//    public Node getNextNode() {return nextNode;}
+//    public void setNextNode(Node nextNode) {this.nextNode = nextNode;}
+//}
+//class Stack{
+//    private int size = 0;
+//    Node top = null;
+//    public boolean isEmpty(){return size == 0;}
+//    public void size(){
+//        System.out.println("Size: " + size);
+//    }
+//    public void push(int data) {
+//        Node newNode = new Node(data);
+//        newNode.setNextNode(top);
+//        top = newNode;
+//        size++;
+//        System.out.println("Add: " + data);
+//    }
+//    public void pop() {
+//        if (isEmpty()) {
+//            System.out.println("Empty");
+//        } else {
+//            Node node = top;
+//            top = top.getNextNode();
+//            size--;
+//            System.out.println("Remove: " + node.getData());
+//        }
+//    }
+//    public void peek(){
+//        if (isEmpty()) {
+//            System.out.println("Empty");
+//        } else {
+//            System.out.println("Top: " + top.getData());
+//        }
+//    }
+//}
+//
+//class Queue {
+//    private int size = 0;
+//    private Node front, rear;
+//
+//    public boolean isEmpty() {
+//        return size == 0;
+//    }
+//
+//    public int size() {
+//        System.out.println("Size: " + size);
+//        return size;
+//    }
+//
+//    public void enqueue(int data) {
+//        Node node = new Node(data);
+//        if (isEmpty()) {
+//            front = node;
+//        } else {
+//            rear.setNextNode(node);
+//        }
+//        rear = node;
+//        size++;
+//        System.out.println("Add: " + data);
+//    }
+//
+//    public void dequeue() {
+//        if (isEmpty()) {
+//            System.out.println("Empty");
+//            rear = null;
+//        } else {
+//            int result = front.getData();
+//            front = front.getNextNode();
+//            size--;
+//            System.out.println("Remove: " + result);
+//        }
+//    }
+//
+//    public void first() {
+//        if (isEmpty()) {
+//            System.out.println("Queue is empty");
+//        } else {
+//            System.out.println("Front: " + front.getData());
+//        }
+//    }
+//}
 
-class Queue {
-    private int size = 0;
-    private Node front, rear;
+abstract class Map<K, V>{
+    class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> prev;
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public int size() {
-        System.out.println("Size: " + size);
-        return size;
-    }
-
-    public void enqueue(int data) {
-        Node node = new Node(data);
-        if (isEmpty()) {
-            front = node;
-        } else {
-            rear.setNextNode(node);
-        }
-        rear = node;
-        size++;
-        System.out.println("Add: " + data);
-    }
-
-    public void dequeue() {
-        if (isEmpty()) {
-            System.out.println("Empty");
-            rear = null;
-        } else {
-            int result = front.getData();
-            front = front.getNextNode();
-            size--;
-            System.out.println("Remove: " + result);
-        }
-    }
-
-    public void first() {
-        if (isEmpty()) {
-            System.out.println("Queue is empty");
-        } else {
-            System.out.println("Front: " + front.getData());
-        }
-    }
-}
-class Map<K, V> {
-    public static class Entry<K, V> {
-        private final K key;
-        private V value;
-        private Entry<K, V> next;
-
-        public Entry(K key, V value, Entry<K, V> next){
+        public Node(K key, V value, Node<K, V> prev) {
             this.key = key;
             this.value = value;
-            this.next = next;
+            this.prev = prev;
         }
-        public K getKey() {return key;}
-        public V getValue() {return value;}
-        public void setValue(V value) {this.value = value;}
-        public Entry getNext() {return next;}
-        public void setNext(Entry<K, V> next) {this.next = next;}
-    }
-    private int size = 10;
-    private final Entry[] table;
-    public Map(){table = new Entry[size];}
-    private int buckets(K key){
-        if(key == null){
-            return 0;
+        @Override
+        public String toString() {
+            return "Node{" + '\n' +
+                    "key=" + key +
+                    ", value=" + value +
+                    ", prev=" + prev +
+                    '}';
         }
-        return Math.abs(key.hashCode() % size);
     }
-    public void put(K key, V value){
-        int index = buckets(key);
-        Entry<K, V> newEntry = new Entry<>(key, value, null);
-        if(table[index] == null)
-            table[index] = newEntry;
+    public Node node = null;
+    public void Map(){
     }
-    public void remove(K key){
-        int index = buckets(key);
-        Entry previous = null;
-        Entry entry = table[index];
-        while (entry != null){
-            if(entry.getKey().equals(key)) {
-                entry = entry.getNext();
-                table[index] = entry;
-                System.out.println("Remove key: " + key);
-                return;
+    void add(K key, V value){
+        // trung key???
+        node = new Node(key, value, node);
+    }
+
+    public boolean containsKey(Object key) {
+        if(key != null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    Object get(K key){
+        for (Node<K, V> currentNode = node; currentNode != null; currentNode = currentNode.prev){
+            if(currentNode.key == key){
+                return currentNode.value;
+
+            //currentNode.prev = null ????
+            }
+            if(currentNode.prev == null){
+                return null;
+            }
+            if(currentNode.prev.key == key){
+                return currentNode.prev.value;
+            }
+
+        }
+        return null;
+    }
+
+    Object remove(K key){
+        for (Node<K, V> currentNode = node; currentNode != null; currentNode = currentNode.prev){
+//            if(currentNode.prev.key == key){
+//                Node<K, V> removeNode = currentNode.prev.prev;
+//                currentNode.prev = removeNode;
+//                return removeNode;
+//            }
+//            if(currentNode == key){
+//                node = node.prev;
+//                return currentNode != null ? currentNode : null;
+//            }
+            if(currentNode.prev.key == key){
+                Node<K, V> removeNode = currentNode.prev.prev;
+                currentNode.prev = removeNode;
+                return removeNode;
+            }else if(currentNode.key == key){
+                currentNode = currentNode.prev;
+                return currentNode != null ? currentNode : null;
+            } else if(currentNode.prev.prev.key == key){
+//                node.prev = node.prev;
             }
         }
+        return null;
     }
-    public void display(){
-        for(int i = 0; i < size; i++){
-            if(table[i] != null){
-                Entry currentNode = table[i];
-                System.out.printf("Key is %s and value is %s %n", currentNode.getKey(), currentNode.getValue());
-            }
-        }
+
+    @Override
+    public String toString() {
+        return "Map{" +
+                "node=" + node +
+                '}';
     }
 }
-
+//
+//node1{
+//    k key1;
+//    v value1;
+//    prev1 -> null;
+//}
+//node2{
+//    k key2;
+//    v value2;
+//    prev2 -> key1;
+//}
+//node3{
+//    k key3;
+//    v value3;
+//    prev3 -> key2;
+//}
+//node4{
+//    k key4;
+//    v value4;
+//    prev4 -> key3;
+//}
+//
+//1.next = 3
+//3.pre = 1
+//
+//n.pre.next = n.next
+//n.next.pre = n.pre
