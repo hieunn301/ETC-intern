ed23be7ea018c24984c7bb4c5ef3234b7eb2f5c7
hieunn301 <95908932+hieunn301@users.noreply.github.com>
2022-12-15 17:02:08 +0700

    done search

--- oop/workspace/Algorithm/MergeSort.java
+++ oop/workspace/Algorithm/MergeSort.java
@@ -1,9 +1,32 @@
-package oop.workspace.Algorithm;
-
-public class MergeSort {
-    static void merge(int array[], int left, int middle, int right){
-        int leftArray = middle - left + 1;
-        int rightArray = right - middle;
-        int 
-    }
-}
+//package oop.workspace.Algorithm;
+//
+//public class MergeSort {
+//    static void mergeSort(int array[]){
+//        int length = array.length;
+//        if (length <= 1) return;
+//
+//        int middle = length / 2;
+//        int[] leftArray = new int[middle];
+//        int[] rightArray = new int[length - middle];
+//
+//        int i = 0;
+//        int j = 0;
+//        for(int i = 0; i < length; i++){
+//            if(i < middle){
+//                leftArray[i] = array[i];
+//            }else{
+//                rightArray[j] = array[i];
+//                j++;
+//            }
+//        }
+//        mergeSort(leftArray);
+//        mergeSort(rightArray);
+//        merge(int array[], int leftArray, int rightArray);
+//    }
+//
+//    static void merge(int[] leftArray, int[] right, int[] array){
+//        int leftSize = array.length / 2;
+//        int rightSize = array.length - leftSize;
+//        int i = 0;
+//    }
+//}
--- oop/workspace/Algorithm/Search.java
+++ oop/workspace/Algorithm/Search.java
@@ -2,23 +2,23 @@ package oop.workspace.Algorithm;
 
 import java.util.Arrays;
 
-class
-LinearSearch{
+class LinearSearch{
     // Danh sách hoặc mảng được duyệt tuần tự và mọi phần tử đều được kiểm tra. Time O(N)
-    static int[] arr = { 5, 15, 6, 9, 4 };
-    static int linearSearch(int arr[], int size, int key) {
+    // duyệt từ cuối mảng, lần lượt duyệt về đầu mảng để tìm ra key cần tìm
+    static int linearSearch(int[] array, int size, int key) {
         if (size == 0) {
             return -1;
-        } else if (arr[size - 1] == key) {
+        } else if (array[size - 1] == key) {
             return size - 1;
         } else {
-            return linearSearch(arr, size - 1, key);
+            return linearSearch(array, size - 1, key);
         }
     }
 
     public static void main(String[] args) {
-        int key = 9;
-        int index = linearSearch(arr, arr.length, key);
+        int[] array = { 5, 15, 6, 9, 4 };
+        int key = 6;
+        int index = linearSearch(array, array.length, key);
         if (index != -1)
             System.out.println("The element " + key + " is found at " + index + " index of the given array.");
         else
@@ -28,124 +28,125 @@ LinearSearch{
 
 class BinarySearch {
     //Chia để trị, chia đôi khoảng thời gian tìm kiếm nhiều lần. Time O (log n)
-    public static int binarySearch(int arr[], int left, int right, int key) {
-        if (left <= right) {
-            int middle = (right - left) / 2;
-            if (arr[middle] == key)
+    public static int binarySearch(int[] array, int left, int right, int key) {
+        if (right >= left) {
+            int middle = left + (right - left) / 2;
+            if (array[middle] == key) {
                 return middle;
-            if (arr[middle] > key)
-                return binarySearch(arr, left, middle - 1, key);
-            if (arr[middle] < key)
-                return binarySearch(arr, middle + 1, right, key);
+            }
+            if (array[middle] > key)
+                return binarySearch(array, left, middle - 1, key);
+            if (array[middle] < key) {
+                return binarySearch(array, middle + 1, right, key);
+            }
         }
         return -1;
     }
 
-    public static void main(String args[]) {
-        BinarySearch object = new BinarySearch();
-        int arr[] = {2, 3, 4, 5, 6 ,7 ,8 ,9,10};
-        int n = arr.length;
-        int key = 10;
-        int result = object.binarySearch(arr, 0, n - 1, key);
-        if (result == -1)
+    public static void main(String[] args) {
+        int[] array = {2, 8, 9, 11 , 13, 14, 18};
+        int key = 14;
+        int index = binarySearch(array, 0, array.length - 1, key);
+        if (index == -1)
             System.out.println("Element not present");
         else
-            System.out.println("Element found at index " + result);
+            System.out.println("Element found at index " + index);
     }
 }
 
 class TernarySearch{
 //Kỹ thuật trong khoa học máy tính dùng để tìm kiếm giá trị lớn nhất (maximum) hay nhỏ nhất (minimum) của một unimodal function
 //Time O (log [n]) trong đó cơ số của log = 3
-    static int ternarySearch(int arr[], int left, int right, int key) {
+    static int ternarySearch(int[] array, int left, int right, int key) {
         if (left <= right) {
             int mid1 = left + (right - left) / 3;
             int mid2 = right - (right - left) / 3;
-            if (arr[mid1] == key) {
+            if (array[mid1] == key) {
                 return mid1;
             }
-            if (arr[mid2] == key) {
+            if (array[mid2] == key) {
                 return mid2;
             }
-            if (key < arr[mid1]) {
-                return ternarySearch(arr, left, mid1 - 1, key);
+            if (key < array[mid1]) {
+                return ternarySearch(array, left, mid1 - 1, key);
             }
-            else if (key > arr[mid2]) {
-                return ternarySearch(arr, mid2 + 1, right, key);
+            else if (key > array[mid2]) {
+                return ternarySearch(array, mid2 + 1, right, key);
             }
             else {
-                return ternarySearch(arr, mid1 + 1, mid2 - 1, key);
+                return ternarySearch(array, mid1 + 1, mid2 - 1, key);
             }
         }
         return -1;
     }
-    public static void main(String args[]) {
-        int left, right, object, key;
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    public static void main(String[] args) {
+        int left, right, index, key;
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         left = 0;
         right = 9;
+
         key = 5;
-        object = ternarySearch(arr, left, right, key);
-        System.out.println("Index of " + key + " is " + object);
-        key = 50;
-        object = ternarySearch(arr, left, right, key);
-        System.out.println("Index of " + key + " is " + object);
+        index = ternarySearch(array, left, right, key);
+        System.out.println("Index of " + key + " is " + index);
+
+        key = 9;
+        index = ternarySearch(array, left, right, key);
+        System.out.println("Index of " + key + " is " + index);
     }
 }
 
 class JumpSearch {
     //Phần tử tìm kiếm sẽ nằm trong khoảng của nhảy mà chứa phần từ lớn hơn giá trị tìm kiếm. Time 0(sqrt(n))
-    public static int jumpSearch(int[] arr, int x) {
-        int n = arr.length;
+    public static int jumpSearch(int[] array, int x) {
+        int n = array.length;
         int step = (int)Math.floor(Math.sqrt(n));
         int prev = 0;
-        while (arr[Math.min(step, n)-1] < x) {
+        while (array[Math.min(step, n) - 1] < x) {
             prev = step;
             step += (int)Math.floor(Math.sqrt(n));
             if (prev >= n)
                 return -1;
         }
-        while (arr[prev] < x) {
+        while (array[prev] < x) {
             prev++;
             if (prev == Math.min(step, n))
                 return -1;
         }
-        if (arr[prev] == x)
+        if (array[prev] == x)
             return prev;
 
         return -1;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610};
-        int x = 55;
-        int index = jumpSearch(arr, x);
+        int[] array = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610};
+        int x = 13;
+        int index = jumpSearch(array, x);
         System.out.println("Number " + x + " is at index " + index);
     }
 }
 
 class InterpolationSearch {
 //Tìm kiếm một từ trong từ điển. Thuật toán tìm kiếm nội suy cải thiện thuật toán tìm kiếm nhị phân.
-// Công thức tìm giá trị là: K = data-low/high-low.
-    public static int interpolationSearch(int arr[], int left, int right, int x) {
-        int position;
-        if (left <= right && x >= arr[left] && x <= arr[right]) {
-            position = left + (((right - left) / (arr[right] - arr[left])) * (x - arr[left]));
-            if (arr[position] == x)
-                return position;
-            if (arr[position] < x)
-                return interpolationSearch(arr, position + 1, right, x);
-            if (arr[position] > x)
-                return interpolationSearch(arr, left, position - 1, x);
+// Công thức tìm giá trị là: K = (data-low/arr[high]-arr[low])*(x - arr[low])
+    public static int interpolationSearch(int[] array, int left, int right, int key) {
+        if ((left <= right) && (key >= array[left]) && (key <= array[right])) {
+            int mid = left + (((right - left) / (array[right] - array[left])) * (key - array[left]));
+            if (array[mid] == key)
+                return mid;
+            if (array[mid] < key)
+                return interpolationSearch(array, mid + 1, right, key);
+            if (array[mid] > key)
+                return interpolationSearch(array, left, mid - 1, key);
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42, 47 };
-        int n = arr.length;
-        int x = 18;
-        int index = interpolationSearch(arr, 0, n - 1, x);
+        int[] array = { 10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42, 47 };
+        int key = 18;
+        int index = interpolationSearch(array, 0, array.length - 1, key);
         if (index != -1)
             System.out.println("Element found at index " + index);
         else
@@ -154,7 +155,7 @@ class InterpolationSearch {
 }
 class ExponentialSearch{
 //Bắt đầu với kích thước mảng con 1, so sánh phần tử cuối cùng của nó với x, sau đó thử kích thước 2, rồi 4. Time O(Log n)
-    static int exponentialSearch(int arr[], int n, int x) {
+    static int exponentialSearch(int[] arr, int n, int x) {
         if (arr[0] == x)
             return 0;
         int i = 1;
@@ -163,11 +164,122 @@ class ExponentialSearch{
         return Arrays.binarySearch(arr, i/2, Math.min(i, n-1), x);
     }
 
-    public static void main(String args[]) {
-        int arr[] = {2, 3, 4, 10, 40};
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 10, 40};
         int x = 10;
         int result = exponentialSearch(arr, arr.length, x);
         System.out.println((result < 0) ? "Element is not present in array" : "Element is present at index " + result);
+    }
+}
+
+class Fibonacci {
+    public static int min(int x, int y) {
+        return (x <= y) ? x : y;
+    }
+
+    public static int fibMonaccianSearch(int[] array, int x, int n) {
+        int fibMMm2 = 0;
+        int fibMMm1 = 1;
+        int fibM = fibMMm2 + fibMMm1;
+        while (fibM < n) {
+            fibMMm2 = fibMMm1;
+            fibMMm1 = fibM;
+            fibM = fibMMm2 + fibMMm1;
+        }
+        int offset = -1;
+        while (fibM > 1) {
+            int i = min(offset + fibMMm2, n - 1);
+            if (array[i] < x) {
+                fibM = fibMMm1;
+                fibMMm1 = fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+                offset = i;
+            }
+            else if (array[i] > x) {
+                fibM = fibMMm2;
+                fibMMm1 = fibMMm1 - fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+            }
+            else
+                return i;
+        }
+        if (fibMMm1 == 1 && array[n-1] == x)
+            return n-1;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 10, 22, 35, 40, 45, 50,
+                80, 82, 85, 90, 100,235};
+        int n = 12;
+        int x = 235;
+        int index = fibMonaccianSearch(array, x, n);
+        if(index >= 0)
+            System.out.print("Found at index: " + index);
+        else
+            System.out.print(x+" isn't present in the array");
+    }
+}
+
+class SublistSearch {
+    static class Node {
+        int data;
+        Node next;
+    };
+    static boolean findList(Node first, Node second) {
+        Node ptr1 = first, ptr2 = second;
+        if (first == null && second == null)
+            return true;
+        if (first == null ||
+                (first != null && second == null))
+            return false;
+        while (second != null) {
+            ptr2 = second;
+            while (ptr1 != null) {
+                if (ptr2 == null)
+                    return false;
+                else if (ptr1.data == ptr2.data) {
+                    ptr1 = ptr1.next;
+                    ptr2 = ptr2.next;
+                }
+                else break;
+            }
+            if (ptr1 == null)
+                return true;
+            ptr1 = first;
+            second = second.next;
+        }
+        return false;
+    }
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.printf("%d ", node.data);
+            node = node.next;
+        }
+    }
+    static Node newNode(int key) {
+        Node temp = new Node();
+        temp.data= key;
+        temp.next = null;
+        return temp;
+    }
+    public static void main(String[] args) {
+        Node a = newNode(1);
+        a.next = newNode(2);
+        a.next.next = newNode(3);
+        a.next.next.next = newNode(4);
+
+        Node b = newNode(1);
+        b.next = newNode(2);
+        b.next.next = newNode(1);
+        b.next.next.next = newNode(2);
+        b.next.next.next.next = newNode(3);
+        b.next.next.next.next.next = newNode(4);
+
+        if(findList(a, b) == true)
+            System.out.println("LIST FOUND");
+        else
+            System.out.println("LIST NOT FOUND");
     }
 }
 
